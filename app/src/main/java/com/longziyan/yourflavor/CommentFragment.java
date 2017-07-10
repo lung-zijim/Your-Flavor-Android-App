94dc9e7c93c1f59b5a364fe9838a8023f8cf068d
lung-zijim <lzylong@gmail.com>
2017-07-10 08:03:34 -0400

    Fragments

---
+++ app/src/main/java/com/longziyan/yourflavor/CommentFragment.java
@@ -0,0 +1,35 @@
+package com.longziyan.yourflavor;
+
+
+import android.os.Bundle;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CommentFragment extends Fragment {
+
+
+    public CommentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_comment, container, false);
+        GridView gridView = (GridView) view.findViewById(R.id.comment_grid);
+        gridView.setAdapter(new RestaurantAdapter(getActivity()));
+        return view;
+
+    }
+
+}
--- app/src/main/java/com/longziyan/yourflavor/RestaurantListActivity.java
+++ app/src/main/java/com/longziyan/yourflavor/RestaurantListActivity.java
@@ -1,5 +1,8 @@
 package com.longziyan.yourflavor;
 
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,21 +10,35 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RestaurantListActivity extends AppCompatActivity {
+    EventFragment listFragment;
+    CommentFragment gridFragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
         Log.e("Life cycle test", "We are at onCreate()");
-        // Get ListView object from xml.
-        ListView restaurantListView = (ListView) findViewById(R.id.restaurant_list);
+        //add list view
+        if (isTablet()) {
+            listFragment = new EventFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.event_container, listFragment).commit();
+        }
 
-        // Initialize an adapter.
-        RestaurantAdapter adapter = new RestaurantAdapter(this);
-
-        // Assign adapter to ListView.
-        restaurantListView.setAdapter(adapter);
+        //add Gridview
+        gridFragment = new CommentFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.comment_container, gridFragment).commit();
     }
+
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+
 
     @Override
     protected void onStart() {
